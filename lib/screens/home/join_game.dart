1dc7e4c0dec6852a0418e3803e4fd9c4034932bb
matthewkhle <47379008+matthewkhle@users.noreply.github.com>
2022-05-02 23:41:22 -0700

    Input validation

--- lib/screens/home/join_game.dart
+++ lib/screens/home/join_game.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:tally/models/game_arguments.dart';
 
 class JoinGame extends StatefulWidget {
@@ -12,65 +13,81 @@ class JoinGame extends StatefulWidget {
 
 class _JoinGameState extends State<JoinGame> {
   final _codeController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        SizedBox(
-          width: 150.0,
-          child: TextField(
-            controller: _codeController,
-            textAlign: TextAlign.center,
-            style: const TextStyle(
-              color: Colors.white,
-            ),
-            decoration: InputDecoration(
-              labelText: 'Enter Code',
-              labelStyle: const TextStyle(
-                color: Colors.white,
-              ),
-              enabledBorder: OutlineInputBorder(
-                borderSide: const BorderSide(
-                  width: 3,
-                  color: Colors.lightBlueAccent,
+    return Expanded(
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              width: 150,
+              child: TextFormField(
+                controller: _codeController,
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.digitsOnly
+                ],
+                style: const TextStyle(
+                  color: Colors.white,
                 ),
-                borderRadius: BorderRadius.circular(15),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: const BorderSide(
-                  width: 3,
-                  color: Colors.lightBlueAccent,
+                decoration: InputDecoration(
+                  labelText: 'Enter Code',
+                  labelStyle: const TextStyle(
+                    color: Colors.white,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      width: 3,
+                      color: Colors.lightBlueAccent,
+                    ),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: const BorderSide(
+                      width: 3,
+                      color: Colors.lightBlueAccent,
+                    ),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
                 ),
-                borderRadius: BorderRadius.circular(15),
-              ),
-              // fillColor: Colors.white,
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: TextButton(
-            style: ButtonStyle(
-              foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-              backgroundColor:
-                  MaterialStateProperty.all<Color>(Colors.lightBlueAccent),
-            ),
-            onPressed: () {
-              Navigator.pushNamed(context, "/scoreboard",
-                  arguments: GameArguments(_codeController.text));
-            },
-            child: const Text(
-              'Join',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+                textAlign: TextAlign.center,
+                validator: (text) {
+                  if (text == null || text.isEmpty) {
+                    return 'Text is empty';
+                  } else if (text.length != 6) {
+                    return 'Enter a 6-digit code';
+                  }
+                  return null;
+                },
               ),
             ),
-          ),
+            TextButton(
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.lightBlueAccent),
+              ),
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  Navigator.pushNamed(context, "/scoreboard",
+                      arguments: GameArguments(_codeController.text));
+                }
+              },
+              child: const Text(
+                'Join',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
